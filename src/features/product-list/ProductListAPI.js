5690f48b2a5a5a08b98593be1d56db900d8e548f
Gowtham Dongari <gowthamdongari@gmail.com>
2023-10-16 09:53:06 -0500

    ecommerce update

--- src/features/product-list/ProductListAPI.js
+++ src/features/product-list/ProductListAPI.js
@@ -1,5 +1,5 @@
 export function fetchCount(amount = 1) {
-  return new Promise(async(resolve) =>{
+  return new Promise((resolve) =>{
     const response = await fetch('http://localhost:8080')
     const data = await response.json()
     resolve({data})
