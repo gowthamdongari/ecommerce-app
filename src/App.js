734ec2da07dc9511daa1f68b1dda829e57db5f8b
Gowtham Dongari <gowthamdongari@gmail.com>
2023-11-03 10:35:15 -0500

    update index and product pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <ProductList></ProductList>
+      <SignupPage></SignupPage>
     </div>
   );
 }
